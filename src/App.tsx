b25c8d848fe56ac96edaa74e08e2ac4a181a0b42
Smit <smitmpatil123@gmail.com>
2025-05-16 19:57:07 +0530

    Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Routes, Route } from 'react-router-dom'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import LandingPage from './Pages/LandingPage'
+import AboutPage from './Pages/AboutPage'
 import TopNavigation from "./components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <TopNavigation />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
     </>
   )
 }
